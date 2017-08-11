8130077ee412f6e74ee8a309b7ea2f67213612be
IsNull <p.buettiker@elderbyte.com>
2017-08-11 15:34:18 +0200

    refactored folder structure, added pipes module

---
+++ src/pipes/index.ts
@@ -0,0 +1,19 @@
+
+import {CommonModule} from "@angular/common";
+import {BytesPipe} from "./bytes.pipe";
+import {NgModule} from "@angular/core";
+import {TimeAgoPipe} from "./time-ago.pipe";
+
+export {BytesPipe} from "./bytes.pipe"
+export {TimeAgoPipe} from "./time-ago.pipe"
+
+@NgModule({
+    declarations: [
+        BytesPipe, TimeAgoPipe
+    ],
+    exports : [
+        BytesPipe, TimeAgoPipe
+    ],
+    imports : [ CommonModule ]
+})
+export class CommonPipesModule { }
