1668b863724d06ed90b500c3a3eebf091fcf3a27
IsNull <p.buettiker@elderbyte.com>
2017-08-11 20:13:36 +0200

    attempt to simplify custom-http module for AOT

--- src/features/custom-http/index.ts
+++ src/features/custom-http/index.ts
@@ -4,31 +4,17 @@ import {CustomHttpService} from './custom-http.service';
 import {CommonModule} from '@angular/common';
 import {JwtAuthModule} from '@elderbyte/ngx-jwt-auth';
 
-
-
 export * from './custom-http.service'
 
-/*
-export interface CustomHttpConfig {
-    tokenName?: string;
-    noJwtError?: boolean;
-    globalHeaders?: Array<Object>;
-}*/
 
 @NgModule({
   imports : [ CommonModule, JwtAuthModule ]
 })
 export class CustomHttpModule {
-
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: CustomHttpModule,
-      providers: [
-        {
-          provide: CustomHttpService,
-          useClass: CustomHttpService,
-        },
-      ]
+      providers: [CustomHttpService]
     };
   }
 }
