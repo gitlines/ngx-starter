e49a0f5b8d8ad2c44f83aea28774a899fa9e9b51
IsNull <p.buettiker@elderbyte.com>
2017-08-14 16:54:58 +0200

    more attempts to fix library issue

--- src/features/index.ts
+++ src/features/index.ts
@@ -5,7 +5,6 @@ export * from './dialogs';
 export * from './errors';
 export * from './global-search';
 export * from './language'
-export * from './sample';
 export * from './side-content';
 export * from './toasts';
 export * from './toolbar';
