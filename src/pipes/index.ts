fcdede96248a0c6d3fe0a113a4f707054a9070c4
IsNull <p.buettiker@elderbyte.com>
2017-08-17 11:09:45 +0200

    introduced weight pipe

--- src/pipes/index.ts
+++ src/pipes/index.ts
@@ -3,16 +3,19 @@ import {CommonModule} from '@angular/common';
 import {BytesPipe} from './bytes.pipe';
 import {NgModule} from '@angular/core';
 import {TimeAgoPipe} from './time-ago.pipe';
+import {WeightPipe} from './weight.pipe';
 
 export {BytesPipe} from './bytes.pipe'
 export {TimeAgoPipe} from './time-ago.pipe'
+export {WeightPipe} from './weight.pipe'
+
 
 @NgModule({
     declarations: [
-        BytesPipe, TimeAgoPipe
+        BytesPipe, TimeAgoPipe, WeightPipe
     ],
     exports : [
-        BytesPipe, TimeAgoPipe
+        BytesPipe, TimeAgoPipe, WeightPipe
     ],
     imports : [ CommonModule ]
 })
