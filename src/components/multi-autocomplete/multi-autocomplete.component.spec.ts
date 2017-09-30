a532baa855f0ae85e2ef6ced7042df67cc416cbb
Pascal Büttiker <pascalbuettiker@bluewin.ch>
2017-09-30 21:40:19 +0200

    added logger-module provider

--- src/components/multi-autocomplete/multi-autocomplete.component.spec.ts
+++ src/components/multi-autocomplete/multi-autocomplete.component.spec.ts
@@ -4,6 +4,7 @@ import { MultiAutocompleteComponent } from './multi-autocomplete.component';
 import {MultiAutoCompleteModule} from './index';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {MATERIAL_COMPATIBILITY_MODE} from '@angular/material';
+import {LoggerModule, NgxLoggerLevel} from 'ngx-logger';
 
 describe('MultiAutocompleteComponent', () => {
   let component: MultiAutocompleteComponent;
@@ -12,7 +13,13 @@ describe('MultiAutocompleteComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [NoopAnimationsModule, MultiAutoCompleteModule],
-        providers: [{provide: MATERIAL_COMPATIBILITY_MODE, useValue: true}]
+        providers: [
+            {provide: MATERIAL_COMPATIBILITY_MODE, useValue: true},
+            LoggerModule.forRoot({
+                level: NgxLoggerLevel.DEBUG,
+                serverLogLevel: NgxLoggerLevel.OFF
+            })
+            ]
     })
     .compileComponents();
   }));
