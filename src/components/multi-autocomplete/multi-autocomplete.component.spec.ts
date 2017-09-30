8deee8b1798fe2bcabfb8ae1b07915708744bcf4
Pascal Büttiker <pascalbuettiker@bluewin.ch>
2017-09-30 18:28:25 +0200

    fixed unit test

--- src/components/multi-autocomplete/multi-autocomplete.component.spec.ts
+++ src/components/multi-autocomplete/multi-autocomplete.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MultiAutocompleteComponent } from './multi-autocomplete.component';
 import {MultiAutoCompleteModule} from './index';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MATERIAL_COMPATIBILITY_MODE} from '@angular/material';
 
 describe('MultiAutocompleteComponent', () => {
   let component: MultiAutocompleteComponent;
@@ -10,7 +11,8 @@ describe('MultiAutocompleteComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [NoopAnimationsModule, MultiAutoCompleteModule]
+      imports: [NoopAnimationsModule, MultiAutoCompleteModule],
+        providers: [{provide: MATERIAL_COMPATIBILITY_MODE, useValue: true}]
     })
     .compileComponents();
   }));
