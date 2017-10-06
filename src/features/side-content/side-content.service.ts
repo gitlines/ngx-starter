84a37606e57a78e4483175b974446e4e74b4c54c
Pascal Büttiker <pascalbuettiker@bluewin.ch>
2017-10-06 12:23:19 +0200

    improved side-content service

--- src/features/side-content/side-content.service.ts
+++ src/features/side-content/side-content.service.ts
@@ -1,6 +1,6 @@
 
 import {Injectable} from "@angular/core";
-import {ActivatedRouteSnapshot, NavigationEnd, Router, RouterStateSnapshot} from "@angular/router";
+import {ActivatedRouteSnapshot, NavigationEnd, NavigationExtras, Router, RouterStateSnapshot} from '@angular/router';
 import {NGXLogger} from 'ngx-logger';
 
 /**
@@ -10,14 +10,17 @@ import {NGXLogger} from 'ngx-logger';
 @Injectable()
 export class SideContentService {
 
+
     /***************************************************************************
      *                                                                         *
      * Fields                                                                  *
      *                                                                         *
      **************************************************************************/
 
-    public navigationOpen = false;
-    public sideContentOpen = false;
+    private _navigationOpen = false;
+    private _sideContentOpen = false;
+    private _detailContentOutlet = 'side';
+    private _clickOutsideToClose = true;
 
 
     /***************************************************************************
@@ -37,11 +40,11 @@ export class SideContentService {
             .map(event => event as NavigationEnd)
             .subscribe(event => {
 
-                if (this.isOutletActive('side')) {
-                    this.logger.debug('Side-Content: "side" outlet is active -> showing side content!');
+                if (this.isOutletActive(this.detailContentOutlet)) {
+                    this.logger.debug(`Side-Content: "${this.detailContentOutlet}" outlet is active -> showing side content!`);
                     this.showSideContent();
                 }else {
-                    this.logger.debug('Side-Content: "side" outlet is NOT active -> HIDING side content!');
+                    this.logger.debug(`Side-Content: "${this.detailContentOutlet}" outlet is NOT active -> HIDING side content!`);
                     this.closeSideContent();
                 }
                 this.closeSideNav();
@@ -50,23 +53,120 @@ export class SideContentService {
 
     /***************************************************************************
      *                                                                         *
+     * Properties                                                              *
+     *                                                                         *
+     **************************************************************************/
+
+    /**
+     * Checks if the side content is currently open
+     * @returns {boolean}
+     */
+    public get sideContentOpen(): boolean {
+        return this._sideContentOpen;
+    }
+
+    public set sideContentOpen(value: boolean) {
+        this._sideContentOpen = value;
+    }
+
+    /**
+     * Checks if the navigation is open
+     * @returns {boolean}
+     */
+    public get navigationOpen(): boolean {
+        return this._navigationOpen;
+    }
+
+    public set navigationOpen(value: boolean) {
+        this._navigationOpen = value;
+    }
+
+    /**
+     * Gets the name of the detail outlet.
+     * Default is 'side'
+     * @returns {string}
+     */
+    public get detailContentOutlet(): string {
+        return this._detailContentOutlet;
+    }
+
+    /**
+     * Sets the name of the detail outlet.
+     * @param {string} value
+     */
+    public set detailContentOutlet(value: string) {
+        this._detailContentOutlet = value;
+    }
+
+    /**
+     * Allow closing the side content by clicking outside of it.
+     * @returns {boolean}
+     */
+    public get clickOutsideToClose(): boolean {
+        return this._clickOutsideToClose;
+    }
+
+
+    public set clickOutsideToClose(value: boolean) {
+        this._clickOutsideToClose = value;
+    }
+
+    /***************************************************************************
+     *                                                                         *
      * Public API                                                              *
      *                                                                         *
      **************************************************************************/
 
-
+    /**
+     * Toggles the side navigation
+     */
     public toggleSidenav() {
         this.navigationOpen = !this.navigationOpen;
     }
 
+    /**
+     * Closes the side navigation
+     */
     public closeSideNav() {
         this.navigationOpen = false;
     }
 
+    /**
+     * Closes the side detail content
+     */
     public closeSideContent() {
         this.logger.debug('Side-Content: Hiding ...');
         this.sideContentOpen = false;
-        this.router.navigate([{outlets: {'side': null}}]);
+
+
+        const command = {};
+        command['outlets'] = {};
+        command['outlets'][this.detailContentOutlet] = null;
+
+        this.router.navigate([
+            command
+            ]);
+    }
+
+    /**
+     * Shows the side content
+     * @param {string[]} args The route arguments / path
+     * @param {boolean} clickOutsideToClose If enabled, the side content can be closed by clicking outside of it.
+     * @param {NavigationExtras} extras
+     * @returns {Promise<boolean>}
+     */
+    public navigateSideContent(args: string[], clickOutsideToClose: boolean, extras?: NavigationExtras): Promise<boolean> {
+
+        this.clickOutsideToClose = clickOutsideToClose;
+
+        const command = {};
+        command['outlets'] = {};
+        command['outlets'][this.detailContentOutlet] = args;
+
+        return this.router.navigate(
+            [command],
+            extras
+        );
     }
 
     /***************************************************************************
