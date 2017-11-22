e937322c2536a06bc3f6e518e3bab6db5a008f51
Pascal Büttiker <pascalbuettiker@bluewin.ch>
2017-11-22 14:49:39 +0100

    cosmetics

--- src/directives/infinitescroll/index.ts
+++ src/directives/infinitescroll/index.ts
@@ -1,9 +1,9 @@
 
-import {NgModule} from "@angular/core";
-import {InfiniteScrollDirective} from "./infinite-scroll.directive";
-import {CommonModule} from "@angular/common";
+import {NgModule} from '@angular/core';
+import {InfiniteScrollDirective} from './infinite-scroll.directive';
+import {CommonModule} from '@angular/common';
 
-export {InfiniteScrollDirective} from "./infinite-scroll.directive"
+export {InfiniteScrollDirective} from './infinite-scroll.directive'
 
 @NgModule({
     declarations: [
