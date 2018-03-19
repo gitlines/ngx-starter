92bbbee36f2fb638cfd8f1fd06f15819643b1fea
IsNull <p.buettiker@elderbyte.com>
2018-03-19 10:13:45 +0100

    updated rollup config new output format

--- gulpfile.js
+++ gulpfile.js
@@ -69,23 +69,25 @@ gulp.task('rollup:fesm', function () {
             // See https://github.com/rollup/rollup/wiki/JavaScript-API#entry
             input: `${buildFolder}/index.js`,
 
-            // Allow mixing of hypothetical and actual files. "Actual" files can be files
-            // accessed by Rollup or produced by plugins further down the chain.
-            // This prevents errors like: 'path/file' does not exist in the hypothetical file system
-            // when subdirectories are used in the `src` directory.
-            allowRealFiles: true,
+            output: {
+                // Allow mixing of hypothetical and actual files. "Actual" files can be files
+                // accessed by Rollup or produced by plugins further down the chain.
+                // This prevents errors like: 'path/file' does not exist in the hypothetical file system
+                // when subdirectories are used in the `src` directory.
+                allowRealFiles: true,
 
+                // A list of IDs of modules that should remain external to the bundle
+                // See https://github.com/rollup/rollup/wiki/JavaScript-API#external
+                external: [
+                    '@angular/core',
+                    '@angular/common'
+                ],
 
-            // A list of IDs of modules that should remain external to the bundle
-            // See https://github.com/rollup/rollup/wiki/JavaScript-API#external
-            external: [
-                '@angular/core',
-                '@angular/common'
-            ],
+                // Format of generated bundle
+                // See https://github.com/rollup/rollup/wiki/JavaScript-API#format
+                format: 'es'
+            }
 
-            // Format of generated bundle
-            // See https://github.com/rollup/rollup/wiki/JavaScript-API#format
-            format: 'es'
         }))
         .pipe(gulp.dest(distFolder));
 });
@@ -103,37 +105,38 @@ gulp.task('rollup:umd', function () {
             // See https://github.com/rollup/rollup/wiki/JavaScript-API#entry
             input: `${buildFolder}/index.js`,
 
-            // Allow mixing of hypothetical and actual files. "Actual" files can be files
-            // accessed by Rollup or produced by plugins further down the chain.
-            // This prevents errors like: 'path/file' does not exist in the hypothetical file system
-            // when subdirectories are used in the `src` directory.
-            allowRealFiles: true,
+            output: {
+                // Allow mixing of hypothetical and actual files. "Actual" files can be files
+                // accessed by Rollup or produced by plugins further down the chain.
+                // This prevents errors like: 'path/file' does not exist in the hypothetical file system
+                // when subdirectories are used in the `src` directory.
+                allowRealFiles: true,
 
-            // A list of IDs of modules that should remain external to the bundle
-            // See https://github.com/rollup/rollup/wiki/JavaScript-API#external
-            external: [
-                '@angular/core',
-                '@angular/common'
-            ],
+                // A list of IDs of modules that should remain external to the bundle
+                // See https://github.com/rollup/rollup/wiki/JavaScript-API#external
+                external: [
+                    '@angular/core',
+                    '@angular/common'
+                ],
 
-            // Format of generated bundle
-            // See https://github.com/rollup/rollup/wiki/JavaScript-API#format
-            format: 'umd',
+                // Format of generated bundle
+                // See https://github.com/rollup/rollup/wiki/JavaScript-API#format
+                format: 'umd',
 
-            // Export mode to use
-            // See https://github.com/rollup/rollup/wiki/JavaScript-API#exports
-            exports: 'named',
+                // Export mode to use
+                // See https://github.com/rollup/rollup/wiki/JavaScript-API#exports
+                exports: 'named',
 
-            // The name to use for the module for UMD/IIFE bundles
-            // (required for bundles with exports)
-            // See https://github.com/rollup/rollup/wiki/JavaScript-API#modulename
-            name: 'ngx-starter',
+                // The name to use for the module for UMD/IIFE bundles
+                // (required for bundles with exports)
+                // See https://github.com/rollup/rollup/wiki/JavaScript-API#modulename
+                name: 'ngx-starter',
 
-            // See https://github.com/rollup/rollup/wiki/JavaScript-API#globals
-            globals: {
-                typescript: 'ts'
+                // See https://github.com/rollup/rollup/wiki/JavaScript-API#globals
+                globals: {
+                    typescript: 'ts'
+                }
             }
-
         }))
         .pipe(rename('ngx-starter.umd.js'))
         .pipe(gulp.dest(distFolder));
