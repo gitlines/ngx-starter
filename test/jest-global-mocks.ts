6057ecda4acff5508e5f3b40525e7d151aeff756
IsNull <p.buettiker@elderbyte.com>
2018-03-19 10:21:49 +0100

    improved unit test config

--- test/jest-global-mocks.ts
+++ test/jest-global-mocks.ts
@@ -1,3 +1,5 @@
+import {} from 'jest';
+
 const mock = () => {
   let storage = {};
   return {
---
+++ test/sample.test.ts
@@ -0,0 +1,16 @@
+import {} from 'jest'
+import {DataContextBuilder} from '../src/common/data/data-context-builder';
+
+/**
+ * Tests
+ */
+describe('Sample Test', () => {
+
+    beforeAll(() => {
+        // Config for all
+    });
+
+    it('Object test', () => {
+        expect(DataContextBuilder.start()).toBeInstanceOf(DataContextBuilder);
+    });
+});
