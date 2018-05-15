25e99b2b5a926fe598691e3071516be1955ea6ce
IsNull <p.buettiker@elderbyte.com>
2018-05-15 08:47:10 +0200

    improved access denied module

--- src/components/access-denied/access-denied.module.ts
+++ src/components/access-denied/access-denied.module.ts
@@ -2,6 +2,8 @@
 import {NgModule} from '@angular/core';
 import {AccessDeniedComponent} from './access-denied.component';
 import {CommonModule} from '@angular/common';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatCardModule, MatIconModule} from '@angular/material';
 
 export { AccessDeniedComponent } from './access-denied.component'
 
@@ -12,6 +14,12 @@ export { AccessDeniedComponent } from './access-denied.component'
     exports : [
         AccessDeniedComponent
     ],
-    imports : [ CommonModule ]
+    imports : [
+        CommonModule,
+
+        MatIconModule, MatCardModule,
+
+        FlexLayoutModule
+    ]
 })
 export class AccessDeniedModule { }
