249475e6fead9a5041da9e8edac0471e53df2318
IsNull <p.buettiker@elderbyte.com>
2018-05-15 09:08:06 +0200

    copy all files which start with _ as raw

--- gulpfile.js
+++ gulpfile.js
@@ -156,7 +156,7 @@ gulp.task('copy:build', function () {
  * 8. Copy package.json from /src to /dist
  */
 gulp.task('copy:manifest', function () {
-    return gulp.src([`${srcFolder}/package.json`])
+    return gulp.src([`${srcFolder}/package.json`, `${srcFolder}/**/_*`])
         .pipe(gulp.dest(distFolder));
 });
 
