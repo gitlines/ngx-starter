49e3a54cf3d217ee740b8007dd649842d9d5f55d
IsNull <p.buettiker@elderbyte.com>
2018-06-14 17:04:26 +0200

    added filter-input directive

--- src/directives/forms/form-support-directives.module.ts
+++ src/directives/forms/form-support-directives.module.ts
@@ -4,18 +4,20 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {DelayedFocusDirective} from './delayed-focus.directive';
 import {TouchedDirective} from './touched.directive';
+import {FilterInputDirective} from './filter-input.directive';
+import {MatFormFieldModule} from '@angular/material';
 
 export {DelayedFocusDirective} from './delayed-focus.directive'
 export {TouchedDirective} from './touched.directive'
-
+export {FilterInputDirective} from  './filter-input.directive'
 
 @NgModule({
     declarations: [
-        DelayedFocusDirective, TouchedDirective
+        DelayedFocusDirective, TouchedDirective, FilterInputDirective
     ],
     exports : [
-        DelayedFocusDirective, TouchedDirective
+        DelayedFocusDirective, TouchedDirective, FilterInputDirective
     ],
-    imports : [ CommonModule ]
+    imports : [ CommonModule, MatFormFieldModule ]
 })
 export class FormSupportDirectivesModule { }
