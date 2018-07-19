a1c2e0a8428c075a173ee30dd9b59d9e85408252
IsNull <p.buettiker@elderbyte.com>
2018-07-19 20:59:18 +0200

    improved demo app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {NgxStarterModule, CommonPipesModule, ExpandToggleButtonModule} from '@elderbyte/ngx-starter';
-import {MatButtonModule, MatCommonModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatCommonModule, MatTabsModule, MatToolbarModule} from '@angular/material';
+import {LanguageModule} from '../../projects/elderbyte/ngx-starter/src/lib/features/language/language.module';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AccessDeniedModule} from '../../projects/elderbyte/ngx-starter/src/lib/components/access-denied/access-denied.module';
+import {SimpleWebStorageModule} from '@elderbyte/ngx-simple-webstorage';
 
 @NgModule({
   declarations: [
@@ -11,16 +18,41 @@ import {MatButtonModule, MatCommonModule, MatToolbarModule} from '@angular/mater
   ],
   imports: [
 
+    // System
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    SimpleWebStorageModule.forRoot(),
+
+    // Material
     MatCommonModule,
     MatToolbarModule,
     MatButtonModule,
+    MatTabsModule,
 
-    BrowserModule,
+    // Library
     NgxStarterModule,
     CommonPipesModule,
-    ExpandToggleButtonModule
+    ExpandToggleButtonModule,
+    AccessDeniedModule,
+
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    }),
+
+    LanguageModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// Because of AOT Compiler
+export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
