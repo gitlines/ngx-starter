72aaf3672fe125d630ca1161a0ac85b8657a5bff
IsNull <p.buettiker@elderbyte.com>
2018-07-19 20:39:26 +0200

    improved demo app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {NgxStarterModule, CommonPipesModule} from '@elderbyte/ngx-starter';
+import {NgxStarterModule, CommonPipesModule, ExpandToggleButtonModule} from '@elderbyte/ngx-starter';
+import {MatButtonModule, MatCommonModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+
+    MatCommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+
     BrowserModule,
     NgxStarterModule,
-    CommonPipesModule
+    CommonPipesModule,
+    ExpandToggleButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
