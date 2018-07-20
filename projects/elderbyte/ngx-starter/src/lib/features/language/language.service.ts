1a6a9ad9641718062d234e507e79ce6679847b28
IsNull <p.buettiker@elderbyte.com>
2018-07-20 17:20:30 +0200

    properly initialize prefered language from web store / browser lang

--- projects/elderbyte/ngx-starter/src/lib/features/language/language.service.ts
+++ projects/elderbyte/ngx-starter/src/lib/features/language/language.service.ts
@@ -6,91 +6,127 @@ import {LoggerFactory} from '@elderbyte/ts-logger';
 @Injectable()
 export class LanguageService {
 
-    /***************************************************************************
-     *                                                                         *
-     * Fields                                                                  *
-     *                                                                         *
-     **************************************************************************/
+  /***************************************************************************
+   *                                                                         *
+   * Fields                                                                  *
+   *                                                                         *
+   **************************************************************************/
 
-    private readonly logger = LoggerFactory.getLogger('LanguageService');
+  private readonly logger = LoggerFactory.getLogger('LanguageService');
 
-    private LANGUAGE_STORAGE_KEY = 'language';
+  private LANGUAGE_STORAGE_KEY = 'language';
 
-    /***************************************************************************
-     *                                                                         *
-     * Constructors                                                            *
-     *                                                                         *
-     **************************************************************************/
+  /***************************************************************************
+   *                                                                         *
+   * Constructors                                                            *
+   *                                                                         *
+   **************************************************************************/
 
-    constructor(
-        private translate: TranslateService,
-        private webStorage: WebLocalStorage
-    ) {
-        this.logger.debug('Initializing language service with webstore: ', webStorage);
+  constructor(
+    private translate: TranslateService,
+    private webStorage: WebLocalStorage
+  ) {
+    this.logger.debug('Initializing language service with webstore: ', webStorage);
+
+    // this.translate.addLangs(['en', 'de']);
+    // this.translate.defaultLang = 'en';
+
+    this.applyPreferredLanguage();
+  }
+
+  /***************************************************************************
+   *                                                                         *
+   * Public API                                                              *
+   *                                                                         *
+   **************************************************************************/
+
+  /**
+   * Returns the key of the currently applied language.
+   *
+   * @returns language key
+   */
+  public get currentLanguage(): string {
+    return this.translate.currentLang;
+  }
+
+  /**
+   * Applies the given language which will immediately get apparent
+   * in the UI.
+   *
+   * @param lang language key
+   */
+  public set currentLanguage(lang: string) {
+    this.setLanguageAndRemember(lang);
+  }
+
+  /**
+   * Returns an array of all available language keys.
+   *
+   * @returns e.g. ['en', 'de', 'fr']
+   */
+  public get languages(): Array<string> {
+    return this.translate.getLangs();
+  }
+
+  /**
+   * Checks if the given language is currently active.
+   *
+   * @param lang language key
+   * @returns true if given language is currently active
+   */
+  public isLanguageActive(lang: string): boolean {
+    return this.currentLanguage === lang;
+  }
+
+  /**
+   * Returns the key of the last applied language.
+   *
+   * @returns language key of last applied language
+   */
+  public get lastConfigured(): string | undefined {
+    return this.lookupPreferredLanguage();
+  }
+
+  /***************************************************************************
+   *                                                                         *
+   * Public API                                                              *
+   *                                                                         *
+   **************************************************************************/
+
+  public lookupPreferredLanguage(): string | undefined {
+    return this.webStorage.getItem(this.LANGUAGE_STORAGE_KEY);
+  }
+
+  public forgetPreferredLanguage(): void {
+    this.webStorage.removeItem(this.LANGUAGE_STORAGE_KEY);
+  }
+
+  public rememberPreferredLanguage(lang: string): void {
+    this.webStorage.setItem(this.LANGUAGE_STORAGE_KEY, lang);
+  }
+
+  public applyPreferredLanguage(): void {
+    const preferred = this.lookupPreferredLanguage();
+    if (preferred) {
+      this.setLanguage(preferred);
+    } else {
+      this.translate.use(this.translate.getBrowserLang());
     }
+  }
 
-    /***************************************************************************
-     *                                                                         *
-     * Public API                                                              *
-     *                                                                         *
-     **************************************************************************/
+  /***************************************************************************
+   *                                                                         *
+   * Private Methods                                                         *
+   *                                                                         *
+   **************************************************************************/
 
-    /**
-     * Returns the key of the currently applied language.
-     *
-     * @returns language key
-     */
-    public get currentLanguage(): string {
-        return this.translate.currentLang;
-    }
+  private setLanguageAndRemember(lang: string): void {
+    this.setLanguage(lang);
+    this.rememberPreferredLanguage(lang);
+  }
 
-    /**
-     * Applies the given language which will immediately get apparent
-     * in the UI.
-     *
-     * @param lang language key
-     */
-    public set currentLanguage(lang: string) {
-        this.setLanguage(lang);
-    }
-
-    /**
-     * Returns an array of all available language keys.
-     *
-     * @returns e.g. ['en', 'de', 'fr']
-     */
-    public get languages(): Array<string> {
-        return this.translate.getLangs();
-    }
-
-    /**
-     * Checks if the given language is currently active.
-     *
-     * @param lang language key
-     * @returns true if given language is currently active
-     */
-    public isLanguageActive(lang: string): boolean {
-        return this.currentLanguage === lang;
-    }
-
-    /**
-     * Returns the key of the last applied language.
-     *
-     * @returns language key of last applied language
-     */
-    public get lastConfigured(): string {
-        return this.webStorage.getItem(this.LANGUAGE_STORAGE_KEY);
-    }
-
-    /***************************************************************************
-     *                                                                         *
-     * Private Methods                                                         *
-     *                                                                         *
-     **************************************************************************/
-
-    private setLanguage(lang: string): void {
-        this.translate.use(lang);
-        this.webStorage.setItem(this.LANGUAGE_STORAGE_KEY, lang);
-    }
+  private setLanguage(lang: string): void {
+    this.translate.use(lang);
+  }
 
 }
