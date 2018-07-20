bff2247aea2909231c168d073358ed8da46678ce
IsNull <p.buettiker@elderbyte.com>
2018-07-20 17:20:10 +0200

    added translation to demo (to showcase language service)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'starter-demo-root',
@@ -9,6 +10,12 @@ export class AppComponent {
 
 
   public searchOpen = false;
-
   public title = 'starter-demo';
+
+  constructor(
+    translateService: TranslateService
+  ) {
+    translateService.addLangs(['en', 'de']);
+    translateService.defaultLang = 'en';
+  }
 }
