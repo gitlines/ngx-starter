8e48b76b1dec6e98037e6b643a59610447e6b61a
IsNull <p.buettiker@elderbyte.com>
2018-07-20 09:52:38 +0200

    cosmetics

--- tools/bundle-scss.ts
+++ tools/bundle-scss.ts
@@ -14,32 +14,32 @@ async function bundleScss() {
   const themePath = base + '/src/_theming.scss';
   const otherPath = base + '/src/**/*.scss';
 
-  const { found, bundledContent, imports } = await new Bundler()
-    .Bundle(themePath, [ otherPath ]);
+const { found, bundledContent, imports } = await new Bundler()
+  .Bundle(themePath, [ otherPath ]);
 
-  if (!found) {
-    console.warn('Could not find scss source files in: ' + process.cwd() + ' ' + themePath);
+if (!found) {
+  console.warn('Could not find scss source files in: ' + process.cwd() + ' ' + themePath);
+}
+
+
+if (imports) {
+  const cwd = process.cwd();
+
+  const filesNotFound = imports
+    .filter(x => !x.found)
+    .map(x => relative(cwd, x.filePath));
+
+  if (filesNotFound.length) {
+    console.error(`SCSS imports failed \n\n${filesNotFound.join('\n - ')}\n`);
+    throw new Error('One or more SCSS imports failed');
   }
+}
 
+console.log('imports:', imports);
 
-  if (imports) {
-    const cwd = process.cwd();
-
-    const filesNotFound = imports
-      .filter(x => !x.found)
-      .map(x => relative(cwd, x.filePath));
-
-    if (filesNotFound.length) {
-      console.error(`SCSS imports failed \n\n${filesNotFound.join('\n - ')}\n`);
-      throw new Error('One or more SCSS imports failed');
-    }
-  }
-
-  console.log('imports:', imports);
-
-  if (found) {
-    await writeFile('./dist/' + library + '/theming.scss', bundledContent);
-  }
+if (found) {
+  await writeFile('./dist/' + library + '/theming.scss', bundledContent);
+}
 }
 
 bundleScss();
