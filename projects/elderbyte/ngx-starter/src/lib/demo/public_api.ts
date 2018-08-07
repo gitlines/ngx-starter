d39e337c757b844acf7cb343a3f20783e544415e
IsNull <p.buettiker@elderbyte.com>
2018-08-07 17:17:28 +0200

    removed demo code

--- projects/elderbyte/ngx-starter/src/lib/demo/public_api.ts
+++
@@ -1,4 +0,0 @@
-
-export * from './ngx-starter.service';
-export * from './ngx-starter.component';
-export * from './ngx-starter.module';
--- projects/elderbyte/ngx-starter/src/public_api.ts
+++ projects/elderbyte/ngx-starter/src/public_api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of ngx-starter
  */
 
-export * from './lib/demo/public_api';
 export * from './lib/pipes/public_api';
 export * from './lib/common/public_api';
 export * from './lib/directives/public_api';
