ab3ab9f0fe0eb10bfb630692e4df627a3e64b688
IsNull <p.buettiker@elderbyte.com>
2018-08-10 17:09:53 +0200

    basic mat table binding support

---
+++ projects/elderbyte/ngx-starter/src/lib/common/data/data-context/mat-data-context-support.ts
@@ -0,0 +1,73 @@
+import {MatPaginator, MatSort} from '@angular/material';
+import {IDataContext, IDataContextActivePage} from './data-context';
+import {Subscription, Unsubscribable} from 'rxjs';
+
+
+export class MatTableSupportBuilder {
+
+  private _matSort: MatSort;
+  private _matPaginator: MatPaginator;
+
+
+  public withSort(matSort: MatSort): this {
+    this._matSort = matSort;
+    return this;
+  }
+
+  public withPaginator(matPaginator: MatPaginator): this {
+    this._matPaginator = matPaginator;
+    return this;
+  }
+
+  public bind(dataContext: IDataContext<any>): MatTableDataContextBinding {
+    return new MatTableDataContextBinding(
+      dataContext,
+      this._matSort,
+      this._matPaginator,
+    );
+  }
+
+}
+
+export class MatTableDataContextBinding implements Unsubscribable {
+
+
+  private readonly _subscriptions: Subscription[] = [];
+
+  constructor(
+    private _dataContext: IDataContext<any>,
+    private _matSort: MatSort,
+    private _matPaginator: MatPaginator
+  ) {
+    this.subscribe();
+  }
+
+  public unsubscribe(): void {
+    this._subscriptions.forEach(s => s.unsubscribe());
+  }
+
+  private subscribe(): void {
+
+    if (this._matSort) {
+      this._subscriptions.push(
+        this._matSort.sortChange.subscribe(
+          sort => this._dataContext.sort = sort
+        )
+      );
+    }
+
+    if (this._matPaginator) {
+
+      const pagedDataContext = this._dataContext as IDataContextActivePage<any>;
+
+      if (pagedDataContext.page !== undefined) {
+        this._subscriptions.push(
+          this._matPaginator.page.subscribe(pageRequest => {
+            pagedDataContext.page = pageRequest;
+          })
+        );
+      }
+    }
+
+  }
+}
