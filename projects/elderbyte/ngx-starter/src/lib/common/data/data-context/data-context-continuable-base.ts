a19cb051ba5339ead80e9548e091cf6689896efb
IsNull <p.buettiker@elderbyte.com>
2018-08-11 10:53:48 +0200

    fixed spacing

--- projects/elderbyte/ngx-starter/src/lib/common/data/data-context/data-context-continuable-base.ts
+++ projects/elderbyte/ngx-starter/src/lib/common/data/data-context/data-context-continuable-base.ts
@@ -13,93 +13,93 @@ import {IDataContextContinuable} from './data-context';
  */
 export abstract class DataContextContinuableBase<T> extends DataContextBase<T> implements IDataContextContinuable<T> {
 
-    /***************************************************************************
-     *                                                                         *
-     * Fields                                                                  *
-     *                                                                         *
-     **************************************************************************/
+  /***************************************************************************
+   *                                                                         *
+   * Fields                                                                  *
+   *                                                                         *
+   **************************************************************************/
 
-    private readonly cblogger: Logger = LoggerFactory.getLogger('DataContextContinuableBase');
+  private readonly cblogger: Logger = LoggerFactory.getLogger('DataContextContinuableBase');
 
-    private _chunkSize: number;
-
-
-    /***************************************************************************
-     *                                                                         *
-     * Constructors                                                            *
-     *                                                                         *
-     **************************************************************************/
+  private _chunkSize: number;
 
 
-    protected constructor(
-        chunkSize: number,
-        indexFn?: ((item: T) => any),
-        localSort?: ((a: T, b: T) => number),
-        localApply?: ((data: T[]) => T[])
-    ) {
-        super(indexFn, localSort, localApply);
-        this._chunkSize = chunkSize;
-    }
-
-    /***************************************************************************
-     *                                                                         *
-     * Public API                                                              *
-     *                                                                         *
-     **************************************************************************/
-
-    public reload(): Observable<any> {
-      // Since continuable data-contexts are appending data,
-      // we need to clear it for a reload.
-      this.clearAll();
-      return super.reload();
-    }
+  /***************************************************************************
+   *                                                                         *
+   * Constructors                                                            *
+   *                                                                         *
+   **************************************************************************/
 
 
-    public loadAll(sorts?: Sort[], filters?: Filter[]): void {
+  protected constructor(
+    chunkSize: number,
+    indexFn?: ((item: T) => any),
+    localSort?: ((a: T, b: T) => number),
+    localApply?: ((data: T[]) => T[])
+  ) {
+    super(indexFn, localSort, localApply);
+    this._chunkSize = chunkSize;
+  }
 
-        this.cblogger.debug('Starting to load all data ...');
+  /***************************************************************************
+   *                                                                         *
+   * Public API                                                              *
+   *                                                                         *
+   **************************************************************************/
 
-        // load first page
-        this.start(sorts, filters)
-            .subscribe(() => {
-                this.cblogger.debug('First page has been loaded. Loading remaining data ...');
-                // load rest in a recursive manner
-                this.loadAllRec();
-            }, err => {
-                this.onError(err);
-                this.cblogger.error('Failed to load first page of load all procedure!', err);
-            });
-    }
+  public reload(): Observable<any> {
+    // Since continuable data-contexts are appending data,
+    // we need to clear it for a reload.
+    this.clearAll();
+    return super.reload();
+  }
 
-    public get chunkSize(): number {
-        return this._chunkSize;
-    }
 
-    public set chunkSize(size: number) {
-        this._chunkSize = size;
-    }
+  public loadAll(sorts?: Sort[], filters?: Filter[]): void {
 
-    /***************************************************************************
-     *                                                                         *
-     * Private Methods                                                         *
-     *                                                                         *
-     **************************************************************************/
+    this.cblogger.debug('Starting to load all data ...');
 
-    protected onChunkSizeChanged(newSize: number): void {  }
+    // load first page
+    this.start(sorts, filters)
+      .subscribe(() => {
+        this.cblogger.debug('First page has been loaded. Loading remaining data ...');
+        // load rest in a recursive manner
+        this.loadAllRec();
+      }, err => {
+        this.onError(err);
+        this.cblogger.error('Failed to load first page of load all procedure!', err);
+      });
+  }
 
-    private loadAllRec(): void {
-        this.loadMore()
-            .subscribe(() => {
-                this.cblogger.debug('Loading data chunk finished, loading next...');
-                this.loadAllRec();
-            }, err => {
-                this.onError(err);
-                this.cblogger.error('Loading all failed!', err);
-            }, () => {
-                this.cblogger.info('All data loaded completely.');
-            });
-    }
+  public get chunkSize(): number {
+    return this._chunkSize;
+  }
 
-    public abstract loadMore(): Observable<any>;
-    public abstract get hasMoreData(): boolean;
+  public set chunkSize(size: number) {
+    this._chunkSize = size;
+  }
+
+  /***************************************************************************
+   *                                                                         *
+   * Private Methods                                                         *
+   *                                                                         *
+   **************************************************************************/
+
+  protected onChunkSizeChanged(newSize: number): void {  }
+
+  private loadAllRec(): void {
+    this.loadMore()
+      .subscribe(() => {
+        this.cblogger.debug('Loading data chunk finished, loading next...');
+        this.loadAllRec();
+      }, err => {
+        this.onError(err);
+        this.cblogger.error('Loading all failed!', err);
+      }, () => {
+        this.cblogger.info('All data loaded completely.');
+      });
+  }
+
+  public abstract loadMore(): Observable<any>;
+  public abstract get hasMoreData(): boolean;
 }
