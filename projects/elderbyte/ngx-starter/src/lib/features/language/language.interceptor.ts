14b06ff194885f308073c588dfa120cfd239de55
IsNull <p.buettiker@elderbyte.com>
2018-08-30 15:04:22 +0200

    fixed language request interceptor

--- projects/elderbyte/ngx-starter/src/lib/features/language/language.interceptor.ts
+++ projects/elderbyte/ngx-starter/src/lib/features/language/language.interceptor.ts
@@ -1,43 +1,71 @@
 
 import {Injectable, Injector} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpParams, HttpRequest} from '@angular/common/http';
 import {TranslateService} from '@ngx-translate/core';
 import {Observable} from 'rxjs';
 
+/**
+ * This interceptor injects the current locale
+ * so the backend know what the current desired locale is of a request.
+ */
 @Injectable()
 export class LanguageInterceptor implements HttpInterceptor {
 
-    private _translate: TranslateService;
+  /***************************************************************************
+   *                                                                         *
+   * Fields                                                                  *
+   *                                                                         *
+   **************************************************************************/
 
-    constructor(
-        private inj: Injector) {
+  private _translate: TranslateService;
+
+  /***************************************************************************
+   *                                                                         *
+   * Constructors                                                            *
+   *                                                                         *
+   **************************************************************************/
+
+  constructor(
+    private inj: Injector) {
+  }
+
+  /***************************************************************************
+   *                                                                         *
+   * Public API                                                              *
+   *                                                                         *
+   **************************************************************************/
+
+  public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    if (this.translate.currentLang) {
+
+      // Keeps the original request params. as a new HttpParams
+      let newParams = new HttpParams({fromString: req.params.toString()});
+
+      newParams = newParams.set('locale', this.translate.currentLang);
+
+      // Clone the request with params instead of setParams
+      // Workaround for https://github.com/angular/angular/issues/18812
+      const requestClone = req.clone({
+        params: newParams
+      });
+
+      return next.handle(requestClone);
     }
 
+    return next.handle(req);
+  }
 
-    private get translate(): TranslateService {
-        if (!this._translate) {
-            this._translate = this.inj.get(TranslateService); // Attempt to fix cyclic dependency
-        }
-        return this._translate;
+  /***************************************************************************
+   *                                                                         *
+   * Private Methods                                                         *
+   *                                                                         *
+   **************************************************************************/
+
+  private get translate(): TranslateService {
+    if (!this._translate) {
+      this._translate = this.inj.get(TranslateService); // Workaround for cyclic dependency
     }
-
-    intercept(
-        req: HttpRequest<any>,
-        next: HttpHandler
-    ): Observable<HttpEvent<any>> {
-
-        /*
-        TODO HttpRequest.clone is duplicating existing query params - disabling for now
-
-        if (this.translate.currentLang) {
-            req = req.clone({
-                setParams: {
-                    locale: this.translate.currentLang
-                }
-            });
-        }
-        */
-
-        return next.handle(req);
-    }
+    return this._translate;
+  }
 }
