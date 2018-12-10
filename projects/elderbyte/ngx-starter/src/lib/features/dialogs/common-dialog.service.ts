ec563994ed4e257b7f157c4cd7659436b77b04b5
IsNull <p.buettiker@elderbyte.com>
2018-12-10 09:43:46 +0100

    cosmetics

--- projects/elderbyte/ngx-starter/src/lib/features/dialogs/common-dialog.service.ts
+++ projects/elderbyte/ngx-starter/src/lib/features/dialogs/common-dialog.service.ts
@@ -7,118 +7,146 @@ import {Observable} from 'rxjs';
 import {flatMap, filter, map} from 'rxjs/operators';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class CommonDialogService {
 
-    constructor(
-        private dialog: MatDialog,
-        private translateService: TranslateService
-    ) { }
 
-    /**
-     * @deprecated Use showConfirm({...}) instead
-     *
-     * @param title
-     * @param message
-     * @param config
-     */
-    public confirm(title: string, message: string, config?: MatDialogConfig): Observable<boolean> {
-      return this.showConfirm({
-        title: title,
-        message: message,
-        config: config
-      });
-    }
+  /***************************************************************************
+   *                                                                         *
+   * Fields                                                                  *
+   *                                                                         *
+   **************************************************************************/
 
 
-    /**
-     * Creates a modal confirmation dialog.
-     *
-     * @param config object for conform dialog
-     */
-    public showConfirm(config: ConfirmDialogConfig): Observable<boolean> {
+  /***************************************************************************
+   *                                                                         *
+   * Constructor                                                             *
+   *                                                                         *
+   **************************************************************************/
 
-        if (!config) { throw new Error('Argument must not be null: config'); }
-
-        const keys = [config.title, config.message];
-
-        return this.translateInterpolatedParams(config.interpolateParams).pipe(
-          flatMap(
-            interpolateParams => {
-              return this.translateService.get(keys, interpolateParams).pipe(
-                flatMap(translated => {
-
-                  const title = translated[config.title];
-                  const message = translated[config.message];
-
-                  // return this.confirm(title, message, config);
-
-                  let dialogRef: MatDialogRef<ConfirmDialogComponent>;
-
-                  dialogRef = this.dialog.open(ConfirmDialogComponent, config.config);
-                  dialogRef.componentInstance.title = title;
-                  dialogRef.componentInstance.message = message;
-                  dialogRef.componentInstance.yesNo = config.yesNo;
-
-                  return dialogRef.afterClosed();
-                })
-              );
-            }
-          )
-        );
-    }
-
-    /**
-     * @deprecated Use showQuestion({...}) instead
-     *
-     * Creates a modal question dialog.
-     *
-     * @param title
-     * @param question
-     * @param config
-     */
-    public question(title: string, question: string, config?: MatDialogConfig): Observable<string> {
-      return this.showQuestion({
-        title: title,
-        question: question,
-        config: config
-      });
-    }
+  constructor(
+    private dialog: MatDialog,
+    private translateService: TranslateService
+  ) { }
 
 
-    /**
-     * Creates a modal question dialog.
-     *
-     * @param config
-     */
-    public showQuestion(config: QuestionDialogConfig): Observable<string> {
+  /***************************************************************************
+   *                                                                         *
+   * Public API                                                              *
+   *                                                                         *
+   **************************************************************************/
 
-        if (!config) { throw new Error('Argument must not be null: config'); }
+  /**
+   * @deprecated Use showConfirm({...}) instead
+   *
+   * @param title
+   * @param message
+   * @param config
+   */
+  public confirm(title: string, message: string, config?: MatDialogConfig): Observable<boolean> {
+    return this.showConfirm({
+      title: title,
+      message: message,
+      config: config
+    });
+  }
 
-        const keys = [config.title, config.question];
 
-      return this.translateInterpolatedParams(config.interpolateParams).pipe(
-        flatMap(interpolateParams => {
+  /**
+   * Creates a modal confirmation dialog.
+   *
+   * @param config object for conform dialog
+   */
+  public showConfirm(config: ConfirmDialogConfig): Observable<boolean> {
+
+    if (!config) { throw new Error('Argument must not be null: config'); }
+
+    const keys = [config.title, config.message];
+
+    return this.translateInterpolatedParams(config.interpolateParams).pipe(
+      flatMap(
+        interpolateParams => {
           return this.translateService.get(keys, interpolateParams).pipe(
             flatMap(translated => {
 
               const title = translated[config.title];
-              const message = translated[config.question];
+              const message = translated[config.message];
+
+              // return this.confirm(title, message, config);
+
+              let dialogRef: MatDialogRef<ConfirmDialogComponent>;
+
+              dialogRef = this.dialog.open(ConfirmDialogComponent, config.config);
+              dialogRef.componentInstance.title = title;
+              dialogRef.componentInstance.message = message;
+              dialogRef.componentInstance.yesNo = config.yesNo;
+
+              return dialogRef.afterClosed();
+            })
+          );
+        }
+      )
+    );
+  }
+
+  /**
+   * @deprecated Use showQuestion({...}) instead
+   *
+   * Creates a modal question dialog.
+   *
+   * @param title
+   * @param question
+   * @param config
+   */
+  public question(title: string, question: string, config?: MatDialogConfig): Observable<string> {
+    return this.showQuestion({
+      title: title,
+      question: question,
+      config: config
+    });
+  }
 
 
-              const dlgConf = config.config || new MatDialogConfig();
-              dlgConf.data = { title: title, question: message };
+  /**
+   * Creates a modal question dialog.
+   *
+   * @param config
+   */
+  public showQuestion(config: QuestionDialogConfig): Observable<string> {
 
-              const dialogRef = this.dialog.open(QuestionDialogComponent, dlgConf);
+    if (!config) { throw new Error('Argument must not be null: config'); }
 
-              return dialogRef.afterClosed()
-                .pipe(filter(response => !!response));
+    const keys = [config.title, config.question];
 
-            }));
-        })
-      );
-    }
+    return this.translateInterpolatedParams(config.interpolateParams).pipe(
+      flatMap(interpolateParams => {
+        return this.translateService.get(keys, interpolateParams).pipe(
+          flatMap(translated => {
+
+            const title = translated[config.title];
+            const message = translated[config.question];
+
+
+            const dlgConf = config.config || new MatDialogConfig();
+            dlgConf.data = { title: title, question: message };
+
+            const dialogRef = this.dialog.open(QuestionDialogComponent, dlgConf);
+
+            return dialogRef.afterClosed()
+              .pipe(filter(response => !!response));
+
+          }));
+      })
+    );
+  }
+
+
+  /***************************************************************************
+   *                                                                         *
+   * Private methods                                                         *
+   *                                                                         *
+   **************************************************************************/
 
   /**
    * Translates a list of params
@@ -128,7 +156,7 @@ export class CommonDialogService {
   private translateInterpolatedParams(interpolateParams: any): Observable<string | any> {
 
     const values = Object.getOwnPropertyNames(interpolateParams)
-          .map(key => interpolateParams[key]);
+      .map(key => interpolateParams[key]);
 
     return this.translateService.get(values).pipe(
       map(translated => {
