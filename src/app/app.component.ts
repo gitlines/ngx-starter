b2a3e5a1367b8752c7c9ce823556f581d086be5d
IsNull <p.buettiker@elderbyte.com>
2019-02-05 08:48:21 +0100

    simplified demo shell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,6 @@
 import { Component } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
-import {EbsSideContentService, EbsToastService} from '@elderbyte/ngx-starter';
-
-export class MenuItem {
-
-    constructor(
-        public icon: string,
-        public name: string,
-        public link: string) {
-    }
-}
+import {EbsToastService} from '@elderbyte/ngx-starter';
 
 
 @Component({
@@ -21,31 +12,16 @@ export class AppComponent {
 
   public title = 'starter-demo';
 
-    public menuItems: MenuItem[];
-
-    constructor(
+  constructor(
     translateService: TranslateService,
     private toastService: EbsToastService
   ) {
     translateService.addLangs(['en', 'de']);
     translateService.defaultLang = 'en';
-
-        this.menuItems = [
-
-          new MenuItem('list', 'Table M/D', 'app/master-detail'),
-            new MenuItem('list', 'Eatables', 'app/eatables'),
-            new MenuItem('person', 'Mixed', 'app/mixed-demo'),
-            new MenuItem('business', 'TB Title', 'app/sub/toolbar-title-demo'),
-            new MenuItem('business', 'Override Title', 'app/sub/override-title'),
-        ];
   }
 
-    public get disableClose(): boolean {
-        return false;
-        // return this.sideContentService.clickOutsideToClose;
-    }
 
-    public markAsFavorite(event: Event): void {
-      this.toastService.pushInfo('Marked as favorite!');
-    }
+  public markAsFavorite(event: Event): void {
+    this.toastService.pushInfo('Marked as favorite!');
+  }
 }
