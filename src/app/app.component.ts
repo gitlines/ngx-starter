a839fd04b33538cb8bdf59d70df2500ee44e1c3a
n0daft <home@ericneher.ch>
2019-02-15 15:45:10 +0100

    removed unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {EbsToastService} from '@elderbyte/ngx-starter';
-import {EbsSideContentService} from '../../projects/elderbyte/ngx-starter/src/lib/features/side-content/ebs-side-content.service';
 
 
 @Component({
@@ -15,27 +14,10 @@ export class AppComponent {
 
   constructor(
     translateService: TranslateService,
-    private sideContentService: EbsSideContentService,
     private toastService: EbsToastService
   ) {
     translateService.addLangs(['en', 'de']);
     translateService.defaultLang = 'en';
   }
 
-
-  public openNav(): void {
-    this.sideContentService.navigationOpen = true;
-  }
-
-  public get leftSideContentOpen(): boolean {
-    return this.sideContentService.navigationOpen;
-  }
-
-  public closeLeftSideContent() {
-    this.sideContentService.closeSideNav();
-  }
-
-  public markAsFavorite(event: Event): void {
-    this.toastService.pushInfo('Marked as favorite!');
-  }
 }
