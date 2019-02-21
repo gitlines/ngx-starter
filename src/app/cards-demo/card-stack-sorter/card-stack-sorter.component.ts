2d4ee19b219cc870590aa19125f65beb871464ad
IsNull <p.buettiker@elderbyte.com>
2019-02-21 15:18:51 +0100

    improved demo

--- src/app/cards-demo/card-stack-sorter/card-stack-sorter.component.ts
+++ src/app/cards-demo/card-stack-sorter/card-stack-sorter.component.ts
@@ -1,26 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CardStack} from '../../../../projects/elderbyte/ngx-starter/src/lib/components/card-organizer/card-stack';
+import {Subscription} from 'rxjs';
+
 
 @Component({
   selector: 'starter-demo-card-stack-sorter',
   templateUrl: './card-stack-sorter.component.html',
   styleUrls: ['./card-stack-sorter.component.scss']
 })
-export class CardStackSorterComponent implements OnInit {
+export class CardStackSorterComponent implements OnInit, OnDestroy {
+
+  /***************************************************************************
+   *                                                                         *
+   * Fields                                                                  *
+   *                                                                         *
+   **************************************************************************/
+
+  private _sub: Subscription;
 
   public stack: CardStack<string>;
+  public currentOrder: string;
 
+  /***************************************************************************
+   *                                                                         *
+   * Constructor                                                             *
+   *                                                                         *
+   **************************************************************************/
 
   constructor() { }
 
-  ngOnInit() {
+  /***************************************************************************
+   *                                                                         *
+   * Life Cycle                                                              *
+   *                                                                         *
+   **************************************************************************/
 
-    this.stack = CardStack.newStack('', '', [
+  public ngOnInit(): void {
+    this.stack = CardStack.newStackSimple([
       'one',
       'two',
-      'three'
+      'three',
+      'four',
+      'five',
     ]);
 
+    this._sub = this.stack.cards.subscribe(
+      cards => this.updateCurrentOrder(cards)
+    );
+
+    this.updateCurrentOrder(this.stack.cardsSnapshot);
   }
+
+  public ngOnDestroy(): void {
+    this._sub.unsubscribe();
+  }
+
+  /***************************************************************************
+   *                                                                         *
+   * Private methods                                                         *
+   *                                                                         *
+   **************************************************************************/
+
+  private updateCurrentOrder(cards: string[]): void {
+    this.currentOrder = cards.join('-');
+  }
+
+
 
 }
