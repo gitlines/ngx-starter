0ecd36f223294ca421dd4f71733211bebeb700be
IsNull <p.buettiker@elderbyte.com>
2019-03-06 19:41:13 +0100

    improved BytesPerSecondFormat with fixed percision

--- projects/elderbyte/ngx-starter/src/lib/common/format/bytes-per-second-format.ts
+++ projects/elderbyte/ngx-starter/src/lib/common/format/bytes-per-second-format.ts
@@ -1,24 +1,24 @@
 
 export class BytesPerSecondFormat {
 
-  public static formatBytesPerSecond(bytesPerSecond: number): string {
+  public static formatBytesPerSecond(bytesPerSecond: number, precision: number = null): string {
 
     const KBPerSecond = bytesPerSecond / 1000;
     const MBPerSecond = KBPerSecond / 1000;
     const GBPerSecond = MBPerSecond / 1000;
 
     if (bytesPerSecond < 500) {
-      return bytesPerSecond + ' Bps';
+      return BytesPerSecondFormat.format(bytesPerSecond, precision || 0) + ' Bps';
     } else if (KBPerSecond < 999) {
-      return KBPerSecond + ' KBps';
+      return BytesPerSecondFormat.format(KBPerSecond, precision || 0) + ' KBps';
     } else if (MBPerSecond < 999) {
-      return MBPerSecond + ' MBps';
+      return BytesPerSecondFormat.format(MBPerSecond, precision || 2) + ' MBps';
     } else {
-      return GBPerSecond + ' GBps';
+      return BytesPerSecondFormat.format(GBPerSecond, precision || 2) + ' GBps';
     }
   }
 
-  public static formatAsBitsPerSecond(bytesPerSecond: number): string {
+  public static formatAsBitsPerSecond(bytesPerSecond: number, precision: number = null): string {
 
     const bitsPerSecond = bytesPerSecond * 8;
 
@@ -27,14 +27,18 @@ export class BytesPerSecondFormat {
     const GbPerSecond = MbPerSecond / 1000;
 
     if (bitsPerSecond < 500) {
-      return bitsPerSecond + ' bps';
+      return BytesPerSecondFormat.format(bitsPerSecond, precision || 0) + ' bps';
     } else if (KbPerSecond < 999) {
-      return KbPerSecond + ' Kbps';
+      return BytesPerSecondFormat.format(KbPerSecond, precision || 0) + ' Kbps';
     } else if (MbPerSecond < 999) {
-      return MbPerSecond + ' Mbps';
+      return BytesPerSecondFormat.format(MbPerSecond, precision || 2) + ' Mbps';
     } else {
-      return GbPerSecond + ' Gbps';
+      return BytesPerSecondFormat.format(GbPerSecond, precision || 2) + ' Gbps';
     }
+  }
+
+  private static format(value: number, precision: number): number {
+    return +value.toFixed(precision);
   }
 
 }
