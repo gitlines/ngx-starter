59cff9e7671dfbff164ebe0cf26cb0b7684d4667
IsNull <p.buettiker@elderbyte.com>
2019-03-09 20:10:36 +0100

    changeDetection OnPush

--- projects/elderbyte/ngx-starter/src/lib/components/access-denied/ebs-access-denied.component.ts
+++ projects/elderbyte/ngx-starter/src/lib/components/access-denied/ebs-access-denied.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'ebs-access-denied',
     templateUrl: './ebs-access-denied.component.html',
-    styleUrls: ['./ebs-access-denied.component.scss']
+    styleUrls: ['./ebs-access-denied.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EbsAccessDeniedComponent implements OnInit {
 
