be865ebea3d43edb5c683e116c6fa2dec2bc0502
IsNull <p.buettiker@elderbyte.com>
2019-03-25 15:06:13 +0100

    use on-push change detection

--- projects/elderbyte/ngx-starter/src/lib/components/forms/search/search-box/elder-search-panel.component.ts
+++ projects/elderbyte/ngx-starter/src/lib/components/forms/search/search-box/elder-search-panel.component.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
   selector: 'elder-search-panel',
   template: '<ng-content></ng-content>',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ElderSearchPanelComponent {
   constructor() { }
--- projects/elderbyte/ngx-starter/src/lib/components/panels/card-panel/elder-card-panel.component.ts
+++ projects/elderbyte/ngx-starter/src/lib/components/panels/card-panel/elder-card-panel.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'elder-card-panel',
   templateUrl: './elder-card-panel.component.html',
-  styleUrls: ['./elder-card-panel.component.scss']
+  styleUrls: ['./elder-card-panel.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ElderCardPanelComponent implements OnInit {
 
